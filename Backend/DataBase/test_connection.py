c2e2196f9b7258bbab617bd0839d281de6cd985a
sthasafal <shresthasafal72@gmail.com>
2025-10-19 06:50:13 -0700

    update schema

---
+++ Backend/DataBase/test_connection.py
@@ -0,0 +1,13 @@
+from Backend.DataBase.database import SessionLocal, engine
+from sqlalchemy import text
+
+def test_connection():
+    try:
+        with engine.connect() as conn:
+            result = conn.execute(text("SELECT NOW();"))
+            print("Database Connected! Current time:", list(result))
+    except Exception as e:
+        print("Database Connection Failed:", e)
+
+if __name__ == "__main__":
+    test_connection()
